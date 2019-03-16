17b63b7b8640d3968b0d9d431aba43c1658dec72
Carlos Eduardo Caballero Burgoa <ccaballero@ehacking.com.bo>
2019-03-16 00:08:45 -0400

    add react components

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-const {app,BrowserWindow}=require('electron');
+const {app,BrowserWindow}=require('electron')
+  , path=require('path')
+  , url=require('url')
+  , isDev=require('electron-is-dev');
 
-app.on('ready',()=>{
-    let window=new BrowserWindow({
-        width:800
-      , height:600
-    });
+let window
+  , create_window=()=>{
+        window=new BrowserWindow({
+            width:900
+          , height:680
+        });
 
-    window.loadFile('index.html');
+        window.loadURL(isDev?'http://localhost:3000':`file://${path.join(__dirname, '../build/index.html')}`);
+        window.on('closed',()=>{
+            window=null
+        });
+    };
+
+app.on('ready',create_window);
+
+app.on('window-all-closed',()=>{
+    if(process.platform!=='darwin'){
+        app.quit();
+    }
+});
+
+app.on('activate',()=>{
+    if(window===null){
+        create_window();
+    }
 });
 
