69bce0175c80a879be899d5d878e0763835a4ffa
Carlos Eduardo Caballero Burgoa <ccaballero@ehacking.com.bo>
2019-03-16 17:07:06 -0400

    completed menubar items

---
+++ config.js
@@ -0,0 +1,6 @@
+const join=require('path').join;
+
+module.exports={
+    temp:join(__dirname,'public','temp')
+};
+
--- main.js
+++ main.js
@@ -37,6 +37,63 @@ let window
                     app.quit();
                 }
             }]
+        },{
+            label:'View'
+          , submenu:[{
+                label:'Toolbar'
+            },{
+                label:'Statusbar'
+            },{
+                type:'separator'
+            },{
+                label:'Fullscreen'
+              , accelerator:'F'
+            },{
+                label:'Double page'
+              , accelerator:'D'
+              , type:'checkbox'
+            },{
+                label:'Manga mode'
+              , accelerator:'M'
+              , type:'checkbox'
+            },{
+                type:'separator'
+            },{
+                label:'Best fit mode'
+              , accelerator:'B'
+              , type:'radio'
+            },{
+                label:'Fit width mode'
+              , accelerator:'W'
+              , type:'radio'
+            },{
+                label:'Fit height mode'
+              , accelerator:'H'
+              , type:'radio'
+            },{
+                type:'separator'
+            },{
+                label:'Rotate 90 degrees CW'
+              , accelerator:'R'
+            },{
+                label:'Rotate 90 degrees CCW'
+              , accelerator:'Shift+R'
+            }]
+        },{
+            label:'Navigation'
+          , submenu:[{
+                label:'First page'
+              , accelerator:'Home'
+            },{
+                label:'Previous page'
+              , accelerator:'PageUp'
+            },{
+                label:'Next page'
+              , accelerator:'PageDown'
+            },{
+                label:'Last page'
+              , accelerator:'End'
+            }]
         }]));
 
         window=new BrowserWindow({
